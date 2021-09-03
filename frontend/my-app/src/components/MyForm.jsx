03c0c47bddd76aa106ad9b315b5314d581d9a6bd
fabio <invillia.fabio.apolonio@ext.gympass.com>
2021-09-02 23:00:53 -0300

    basic functionality

--- frontend/my-app/src/components/MyForm.jsx
+++ frontend/my-app/src/components/MyForm.jsx
@@ -20,7 +20,19 @@ function MyForm() {
         headers: { 'Content-Type': 'application/json' },
       })
       .then(res => res.json())
-      .then(json => console.log(json));
+      .then(data => {
+        setNotes(notes => [...notes, data])  
+      });
+  }
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:8080/notes/${id}`, {
+      method: 'delete',
+    })
+    .then(res => res.json())
+
+    const newNotes = notes.filter(note => note.id !== id)
+    setNotes(newNotes)
   }
   
   useEffect( () =>  {
@@ -30,23 +42,38 @@ function MyForm() {
   }, notes)
 
   return (
-      <div className="center">
-        <form onSubmit={handleSubmit} >
-            <label>
-                Title:
-                <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
-            </label>
-            <label>
-                Content:
-                <input type="text" value={content} onChange={e => setContent(e.target.value)}/>
-            </label>
-            <input type="submit" value="Submit"/>
-        </form>
-        <p/>
-        <div className="center">
-            {notes.map( n =><p>{n.title}</p> )}            
+    <div class="container pt-3 my-3 border" >
+      <form onSubmit={handleSubmit} >
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Title</label>
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="title" value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
-      </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Content</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={content} onChange={e => setContent(e.target.value)}></textarea>
+        </div>
+        <div class="mb-3">
+          <input class="form-control" type="submit" value="Submit"/>
+        </div>
+      </form>
+
+        <div class="container-fluid p-3 my-3 border">
+          <div class="d-flex flex-wrap">
+            {notes.map(n => {
+              return (
+                <div key={n.id} class="card m-2" >
+                  <div class="card-header">{n.title}</div>
+                  <div class="card-body">{n.content}</div>
+                  <div class="card-footer">
+                     <i class="bi bi-pencil-square"></i>
+                     <i class="bi bi-trash" onClick={ () => handleDelete(n.id)}></i>
+                  </div>
+                </div>              
+              )
+            })}
+          </div>      
+      </div>      
+    </div>      
   );
 }
 
