1a5742c8ca47abc95a60c0607568f609d7b09445
fabio <invillia.fabio.apolonio@ext.gympass.com>
2021-09-03 07:59:06 -0300

    finish basic funcionality

--- frontend/my-app/src/components/MyForm.jsx
+++ frontend/my-app/src/components/MyForm.jsx
@@ -2,6 +2,7 @@ import './MyForm.css';
 import React, { useState, useEffect } from 'react';
 
 function MyForm() {
+  const [id, setId] = useState(0);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [notes, setNotes] = useState([]);
@@ -14,15 +15,24 @@ function MyForm() {
           "content": content
       }
 
-      fetch('http://localhost:8080/notes', {
-        method: 'post',
-        body:    JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' },
-      })
-      .then(res => res.json())
-      .then(data => {
-        setNotes(notes => [...notes, data])  
-      });
+      if (id) {
+        fetch(`http://localhost:8080/notes/${id}`, {
+          method: 'put',
+          body:    JSON.stringify(body),
+          headers: { 'Content-Type': 'application/json' },
+        })
+        .then(res => res.json())
+      } else {
+        fetch('http://localhost:8080/notes', {
+          method: 'post',
+          body:    JSON.stringify(body),
+          headers: { 'Content-Type': 'application/json' },
+        })
+        .then(res => res.json())  
+      }
+      setId(0)
+      setTitle('')
+      setContent('')
   }
 
   const handleDelete = (id) => {
@@ -30,16 +40,22 @@ function MyForm() {
       method: 'delete',
     })
     .then(res => res.json())
-
-    const newNotes = notes.filter(note => note.id !== id)
-    setNotes(newNotes)
   }
-  
+
+  const handleEdit = (id) => {
+    fetch(`http://localhost:8080/notes/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      setId(data.id)
+      setTitle(data.title)
+      setContent(data.content)
+    });
+  }  
   useEffect( () =>  {
     fetch('http://localhost:8080/notes')
     .then(response => response.json())
     .then(data => setNotes(data));
-  }, notes)
+  })
 
   return (
     <div class="container pt-3 my-3 border" >
@@ -53,10 +69,14 @@ function MyForm() {
           <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={content} onChange={e => setContent(e.target.value)}></textarea>
         </div>
         <div class="mb-3">
-          <input class="form-control" type="submit" value="Submit"/>
+          <input class="form-control" type="submit" value="Save"/>
         </div>
       </form>
-
+        <div class="container p-3 my-3 border">
+          <div class="d-flex justify-content-center">
+            <label>Notes</label>
+          </div>
+        </div>        
         <div class="container-fluid p-3 my-3 border">
           <div class="d-flex flex-wrap">
             {notes.map(n => {
@@ -65,8 +85,10 @@ function MyForm() {
                   <div class="card-header">{n.title}</div>
                   <div class="card-body">{n.content}</div>
                   <div class="card-footer">
-                     <i class="bi bi-pencil-square"></i>
-                     <i class="bi bi-trash" onClick={ () => handleDelete(n.id)}></i>
+                    <div class="d-flex justify-content-around">
+                      <i class="bi bi-pencil-square" onClick={ () => handleEdit(n.id)}></i>
+                      <i class="bi bi-trash" onClick={ () => handleDelete(n.id)}></i>
+                    </div>
                   </div>
                 </div>              
               )
